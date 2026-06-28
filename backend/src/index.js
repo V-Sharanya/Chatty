6caa4097ae5d54004903cd0c3f70b63da746524a
V-Sharanya <sharanyav.146@gmail.com>
2026-06-28 23:50:31 +0530

    create database in mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,19 @@
 import express from "express";
-import authRoutes from './routes/auth.route.js'
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
 
 const app = express();
 
-app.use("api/auth",authenticate)
-app.listen(5001, () => {
-    console.log("Hello");
+const PORT = process.env.PORT;
+
+app.use("/api/auth",authRoutes)
+;
+app.listen(PORT, () => {
+    console.log("Server is running on PORT:" + PORT);
+    connectDB();
 })
