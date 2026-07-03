8a5002e18a07ba1fa8432c77e2e2461c3599fe06
V-Sharanya <sharanyav.146@gmail.com>
2026-07-03 23:49:33 +0530

    started message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 
@@ -11,10 +12,12 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
-;
+app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
+
 app.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT);
     connectDB();
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware";
+import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get(":/id", protectRoute, getMessages);
+export default router;
